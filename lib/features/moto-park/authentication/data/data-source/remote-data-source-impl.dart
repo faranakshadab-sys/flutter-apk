e89701e682faa7648767dec00da110bf4ff824d0
faranakshadab-sys <faranak.shadab@gmail.com>
2026-05-26 00:17:02 +0330

    updated the DIO exception

--- lib/features/moto-park/authentication/data/data-source/remote-data-source-impl.dart
+++ lib/features/moto-park/authentication/data/data-source/remote-data-source-impl.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' hide DioException;
 import 'package:get/get.dart';
 import 'package:parkingandroid/core/utilities/app-connectivity.dart';
 import 'package:parkingandroid/core/utilities/app-dio.dart';
